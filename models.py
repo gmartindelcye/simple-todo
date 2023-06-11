30dbd50676fad5778fc35d814dfddf24468a84d9
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2023-06-11 14:37:00 -0600

    models defined

---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from db import Base
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(String, primary_key=True, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False) 
+    username = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    tasks = relationship("Task", back_populates="user")
+class Task(Base):
+    __tablename__ = "task"
+
+    id = Column(String, primary_key=True, unique=True, index=True, nullable=False)
+    text = Column(String, index=True, nullable=False)
+    user_id = Column(String, ForeignKey("user.id"), nullable=False)
+
+    user = relationship("User", back_populates="task")
+    
